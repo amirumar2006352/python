6e4dacebfb29b3d529b2d869ec0c671517cc8e61
amirumar2006352 <amirumarshrf2006@gmail.com>
2025-05-06 10:54:39 +0530

    Delete 14.4.25 directory

--- 14.4.25/construct the rectangle.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def constructRectangle(self, area):
-        import math
-        w = int(math.sqrt(area))
-        while area % w:
-            w -= 1
-        return [area // w, w]
