87ca69f9fd6160da03cde1944800ff2f9a69871a
amirumar2006352 <amirumarshrf2006@gmail.com>
2025-05-06 10:54:09 +0530

    Delete 11.4.25 directory

--- 11.4.25/find the difference.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def findTheDifference(self, s, t):
-        res = 0
-        for c in s + t:
-            res ^= ord(c)
-        return chr(res)
--- 11.4.25/is subsequence.py
+++
@@ -1,4 +0,0 @@
-class Solution(object):
-    def isSubsequence(self, s, t):
-        it = iter(t)
-        return all(c in it for c in s)
